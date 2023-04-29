0f37264e3eae9ebc3f035d0aa497ec56a1453731
Himanshu Sharma <sweetiehimanshu@gmail.com>
2023-04-29 20:08:01 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: `https://644d28f71e322e1e4f3bfa16--heartfelt-lokum-93dbd2.netlify.app/`
+    origin: `https://644d28f71e322e1e4f3bfa16--heartfelt-lokum-93dbd2.netlify.app`
 }));
 
 app.get(`/home`, (req, res, next) => {
