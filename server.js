ac76941629ca0e7a0eab6cf746c5848e7cdf739b
Himanshu Sharma <sweetiehimanshu@gmail.com>
2023-04-29 19:46:59 +0530

    first commit

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-const http = require('http');
+const express = require('express');
 const cors = require('cors');
 
-const server = http.createServer((req, res) => {
-    const headers = {
-        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 2592000, // 30 days
-        /** add other headers as per requirement */
-    };
+const port = process.env.PORT || 5000;
 
-    res.writeHead(200, headers);
-    res.end('Hii');
+const app = express();
+
+app.get(`/home`, (req, res, next) => {
+    res.send(`Hii`);
 })
 
-const port = 5000;
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 })
