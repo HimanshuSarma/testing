479701fa2a9b44fb1ddaaa46adf260add4bf99b0
Himanshu Sharma <sweetiehimanshu@gmail.com>
2023-04-29 20:01:05 +0530

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: `http://localhost:3000`
+}));
 
 app.get(`/home`, (req, res, next) => {
     res.send(`Hii`);
